46673d531c68695839b107c84dfe00f5503ab079
up1 <somkiat.p@gmail.com>
2026-04-30 16:20:29 +0700

    Refactor page object

--- day2_test/for_ios/test/pageobjects/home.page.ts
+++ day2_test/for_ios/test/pageobjects/home.page.ts
@@ -2,16 +2,21 @@ import { $ } from '@wdio/globals'
 
 class HomePage {
 
+    private elements = {
+        welcomeText: "accessibility id:home_welcome_text2",
+        loginButton: "accessibility id:home_login_button",
+    };
+
     public async pageLodedSuccess() {
         // Check if the welcome text is displayed
-        const el1 = await $("accessibility id:home_welcome_text2");
+        const el1 = await $(this.elements.welcomeText);
         await expect(el1).toBeExisting();
         await expect(el1).toHaveText("Welcome to the App");
     }
 
     public async openLoginPage() {
         // Click on the login button
-        const el3 = await $("accessibility id:home_login_button");
+        const el3 = await $(this.elements.loginButton);
         await el3.click();
     }
 }
